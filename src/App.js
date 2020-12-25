79a2bf0ebe8f2ee3ab1a3f7b7c37c65333b02332
Himayan <vipl2021l149@Admins-MacBook-Pro.local>
2020-12-26 01:43:52 +0530

    feat(ui): flap open animation

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="envlope-wrapper">
+        <div className="envelope">
+          <div className="front flap"></div>
+          <div className="front pocket"></div>
+          <div className="letter">
+            <div className="words line1"></div>
+            <div className="words line2"></div>
+            <div className="words line3"></div>
+            <div className="words line4"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
