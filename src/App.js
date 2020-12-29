df4870fd6a90e2a0c9876d6cbd2ae159b21b9111
Ankita Chakraborty <ankitachakraborty412@gmail.com>
2020-12-30 02:26:39 +0530

    fix: something

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import "./styles/app.scss";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isFalling, setIsFalling] = useState(false);
+  function openEnv() {
+    if(!isOpen) {
+      setIsOpen(true);
+      setTimeout(() => {
+        setIsFalling(true);
+      }, 1500);
+    }
+  }
   return (
     <div className="app">
-      <div className={`envlope-wrapper${isOpen ? ' open' : ''}`} onClick={() => {setIsOpen(!isOpen)}}>
+      <div
+        className={`envlope-wrapper${isOpen ? ' open' : ''}${isFalling ? ' falling' : ''}`} 
+        onClick={openEnv}>
         <div className="envelope">
           <div className="front flap" />
           <div className="front pocket" />
