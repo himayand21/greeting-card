b00b6bee87662a4c5407b8212b5c5cfb81257844
Ankita Chakraborty <ankitachakraborty412@gmail.com>
2020-12-30 00:05:58 +0530

    free-fall animation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import { useState } from "react";
 import "./styles/app.scss";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="app">
-      <div className="envlope-wrapper">
+      <div className={`envlope-wrapper${isOpen ? ' open' : ''}`} onClick={() => {setIsOpen(!isOpen)}}>
         <div className="envelope">
           <div className="front flap"></div>
           <div className="front pocket"></div>
-          <div className="letter">
+        </div>
+        <div className="letter">
           
           </div>
-        </div>
       </div>
     </div>
   );
