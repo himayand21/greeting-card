46ef0ff2220b5cb7d14c5c2dcb1248773def84ff
Ankita Chakraborty <ankitachakraborty412@gmail.com>
2020-12-29 23:42:59 +0530

    fix: organised sass

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import "./App.scss";
+import "./styles/app.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <div className="envlope-wrapper">
         <div className="envelope">
           <div className="front flap"></div>
           <div className="front pocket"></div>
           <div className="letter">
-            <div className="words line1"></div>
-            <div className="words line2"></div>
-            <div className="words line3"></div>
-            <div className="words line4"></div>
+          
           </div>
         </div>
       </div>
