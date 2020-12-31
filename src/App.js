b19daf96da42fce507ec19497793b179ddd83360
Ankita Chakraborty <ankitachakraborty412@gmail.com>
2020-12-31 12:51:04 +0530

    fix: card flip

--- src/App.js
+++ src/App.js
@@ -25,7 +25,16 @@ function App() {
           <div className="front pocket" />
           <div className="back" />
         </div>
-        <div className="letter" />
+        <div className="letter">
+          <div className="letter-face front">
+            <p>Wish you a very happy new year from your favourite teacher. I hope you stay safe and progressive.
+              Blah and blah blah blah and blah
+            </p>
+          </div>
+          <div className="letter-face back">
+
+          </div>
+        </div>
       </div>
     </div>
   );
